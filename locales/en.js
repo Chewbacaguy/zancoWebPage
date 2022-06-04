81fec075673a51844eb08fc5b9a71247880ee9ea
vergaraaa <edgarvm13@hotmail.com>
2022-06-04 09:54:14 -0500

    feat: home with two languages

---
+++ locales/en.js
@@ -0,0 +1,16 @@
+export default {
+    navAbout: 'About',
+    navContact: 'Contact',
+    navLogin: 'Login',
+    homeMessage: 'At Zanco we integrate all the packaging needs with the goal of creating efficient industrial manufacturing. We understand the importance of the quality that each product deserves, from the time it leaves the warehouse, until it reaches its destination',
+    homeWhatWeDoQuestion: 'What we do',
+    homeWhatWeDoAnswer: '<br>We understand the logistical difficulties involved in integrating the respective materials necessary to pack any product.<br><br>That is why our integrator system is in charge of managing the needs of your manufacturing procedures, in order to facilitate all the single channel packing operations.',
+    homeHowDoesItWorkQuestion: 'How does it work?',
+    homeCoffe: 'It\'s easy as ordering a coffee!',
+    homeHowDoesItWorkAnswer: 'Everything revolves around the digital brain called Integral Engine (IE)',
+    homeRegistryStep1: 'Choose your area of interest',
+    homeRegistryStep2: 'Each area has a simple forms where you can register a new packaging order of a specific category.',
+    homeRegistryStep3: 'Each order will be registered within our system where you will be able check the status of each orderd, it\'s progress and follow-ups.',
+    homeRegistryStep4: 'You will be assigned an advisor with whom you can negotiate and review the progress of each order directly.',
+    homeCEOMessage: 'Streamlining processes is our <span class=\'color: rgba(130, 130, 130, 1);\'>specialty</span>, at Zanco we understand that packaging is an <span className={`${styles.quoteHighlight}`}> important</span> and <span class=\'color: rgba(130, 130, 130, 1);\'>necessary</span> need for all manufacturing.'
+}
